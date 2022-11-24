4ef8ed12b60942be808225f1c516d1a6ae5b0f89
w0itke <w.kiezik@gmail.com>
2022-11-24 14:02:21 +0100

    dockerrun file

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <div>
+        <h1>Fib Calculator</h1>
         <header>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
